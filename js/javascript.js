ec8990d2575d9c844034c8e8eecb2d6a5d1b4276
GreyPit <greypit88@gmail.com>
2015-12-02 11:43:52 +0100

    Testing 1 2 3

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+function rotateImage() {
+		$("#image1").toggleClass("imageHidden");
+		$("#image2").toggleClass("imageHidden");
+
+		$("#panelHeading1").toggleClass("imageHidden");
+		$("#panelBody1").toggleClass("imageHidden");
+
+		$("#panelHeading2").toggleClass("imageHidden");
+		$("#panelBody2").toggleClass("imageHidden");
+}
